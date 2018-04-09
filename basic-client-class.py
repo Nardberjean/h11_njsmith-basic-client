451570aceb172bbd7c17d469c4e285bbb0fbb10c
Nardberjean <Nardberjean@users.noreply.github.com>
2018-04-09 07:07:28 -0400

    Update basic-client-class.py

--- basic-client-class.py
+++ basic-client-class.py
@@ -2,61 +2,74 @@ import socket
 import ssl
 import h11
 
+class BasicClient:
+    """A simple example class"""
+    h11conn = h11.Connection(our_role=h11.CLIENT)
+
+    def __init__(self, host, port=443):
+        ctx = ssl.create_default_context()
+        self.sock = ctx.wrap_socket(socket.create_connection((host, port)),
+            server_hostname=host)
+
+    def __exit__(self, *exc_info):
+        print("Closing socket")
+        self.sock.close()
+
+    def send(event):
+        print("Sending event:")
+        print(event)
+        print()
+        # Pass the event through h11's state machine and encoding machinery
+        # Send the resulting bytes on the wire
+        self.sock.sendall(self.h11conn.send(event))
+
+    def next_event():
+        while True:
+            # Check if an event is already available
+            event = self.h11conn.next_event()
+            if event is h11.NEED_DATA:
+                # Nope, so fetch some data from the socket...
+                # ...and give it to h11 to convert back into events...
+                self.h11conn.receive_data(self.sock.recv(2048))
+                # ...and then loop around to try again.
+                continue
+            return event
+
 ################################################################
 # Setup
 ################################################################
 
-h11conn = h11.Connection(our_role=h11.CLIENT)
-ctx = ssl.create_default_context()
-sock = ctx.wrap_socket(socket.create_connection(("httpbin.org", 443)),
-                       server_hostname="httpbin.org")
-
+with BasicClient("httpbin.org") as client:
+      
 ################################################################
 # Sending a request
 ################################################################
 
-def send(event):
-    print("Sending event:")
-    print(event)
-    print()
-    # Pass the event through h11's state machine and encoding machinery
-    # Send the resulting bytes on the wire
-    sock.sendall(h11conn.send(event))
 
-send(h11.Request(method="GET",
-                 target="/get",
-                 headers=[("Host", "httpbin.org"),
-                          ("Connection", "close")]))
-send(h11.EndOfMessage())
+    client.send(h11.Request(method="GET",
+        target="/get",
+        headers=[
+            ("Host", "httpbin.org"),
+            ("Connection", "close")
+        ]))
+    send(h11.EndOfMessage())
 
 ################################################################
 # Receiving the response
 ################################################################
 
-def next_event():
     while True:
-        # Check if an event is already available
-        event = h11conn.next_event()
-        if event is h11.NEED_DATA:
-            # Nope, so fetch some data from the socket...
-            # ...and give it to h11 to convert back into events...
-            h11conn.receive_data(sock.recv(2048))
-            # ...and then loop around to try again.
-            continue
-        return event
-
-while True:
-    event = next_event()
-    print("Received event:")
-    print(event)
-    print()
-    if type(event) is h11.EndOfMessage:
-        break
+        event = client.next_event()
+        print("Received event:")
+        print(event)
+        print()
+        if type(event) is h11.EndOfMessage:
+            break
 
 ################################################################
 # Clean up
 ################################################################
 
-sock.close()
+print("The End")
 
 # Copyed and adapted from https://github.com/python-hyper/h11/blob/master/examples/basic-client.py
